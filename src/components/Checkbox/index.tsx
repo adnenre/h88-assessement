39c8229e49e4829db0e6729120c563855affc298
adnen rebai <adnen.rebai@gmail.com>
2022-11-25 03:47:23 +0100

    refactor : enhance css

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -7,7 +7,7 @@ const Checkbox = ({id,name,checked,onChange}:checkBoxType) => {
         onChange({id,name,checked})
     }
     return (
-        <label className="inline-flex items-center ">
+        <label className="inline-flex items-center px-1">
             <input id={id} type="checkbox" className="form-checkbox h-3 w-3 text-gray-600" onChange={handleChange} checked={checked} name={name} /><span className="ml-2 text-gray-700">{name}</span>
         </label>
     )
