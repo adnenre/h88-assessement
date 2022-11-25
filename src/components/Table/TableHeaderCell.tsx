e78d935e7814466c198d458c1c0c86c403caaec6
adnen rebai <adnen.rebai@gmail.com>
2022-11-25 23:17:18 +0100

    refactor : center header cell

--- src/components/Table/TableHeaderCell.tsx
+++ src/components/Table/TableHeaderCell.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
 type tableCellProps = {
-    children : React.ReactNode
+  children: React.ReactNode
+  onClick: (e: React.MouseEvent<HTMLElement>) => void
 }
-const TableHeaderCell = ({children,...rest}:tableCellProps) => {
+const TableHeaderCell = ({ children, onClick, ...rest }: tableCellProps) => {
   return (
-    <th className='sticky top-0 px-5 py-5 bg-blue-500 text-white border-b dark:border-gray-500 border-gray-200  text-left text-sm uppercasefont-semibold dark:bg-gray-800 dark:text-white text-center' {...rest}>{children}</th>
+    <th onClick={onClick} className=' cursor-pointer sticky top-0 px-5 py-5 bg-blue-400 hover:bg-blue-300 text-white border-b dark:border-gray-500 border-gray-200  text-left text-sm uppercasefont-semibold dark:bg-gray-800 dark:text-white ' {...rest}>
+      <div className="flex">
+        <p className='flex m-auto'>
+          {children}
+        </p>
+      </div>
+    </th>
   )
 }
 
