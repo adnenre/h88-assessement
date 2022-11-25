4cfbe2e2d2ca486e41e6734ad0e896b134d78e2e
adnen rebai <adnen.rebai@gmail.com>
2022-11-25 03:31:03 +0100

    feat : create Checkbox component

---
+++ src/components/Checkbox/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import { checkBoxType } from '../Types'
+
+const Checkbox = ({id,name,checked,onChange}:checkBoxType) => {
+    const handleChange = (e:React.ChangeEvent<HTMLInputElement>) =>{
+        const { id , checked ,name } = e.target
+        onChange({id,name,checked})
+    }
+    return (
+        <label className="inline-flex items-center ">
+            <input id={id} type="checkbox" className="form-checkbox h-3 w-3 text-gray-600" onChange={handleChange} checked={checked} name={name} /><span className="ml-2 text-gray-700">{name}</span>
+        </label>
+    )
+}
+
+export default Checkbox;
